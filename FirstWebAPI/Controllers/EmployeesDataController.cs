8fc329a3d62a6abfc2ebfd4f46ccfb5f7d9625a8
amitpandey02 <amitpandey@nimapinfotech.com>
2023-03-27 14:56:27 +0530

    Initial

---
+++ FirstWebAPI/Controllers/EmployeesDataController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FirstWebAPI.Controllers
+{
+    public class EmployeesDataController : ApiController
+    {
+        public string[] getemployee = { "amit","pandey","jnp" };
+        [HttpGet]
+        public string[] GetEmployee()
+        {
+            return getemployee;
+        }
+        [HttpGet]
+        //We can not pass parameter as index because in webconfig there is optional parameter  name as id ...
+        //public string GetEmployeeByIndex(int index)
+        //{
+        //    return getemployee[id];
+
+        //}
+        public string GetEmployeeByIndex(int id) 
+        {
+            return getemployee[id];
+
+        }
+    }
+}
+
